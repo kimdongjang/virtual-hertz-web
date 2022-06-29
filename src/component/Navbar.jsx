fd2724a5d81afe27804d1028cc3a915c717c7717
jwk <jwk@power21.co.kr>
2022-06-29 11:21:47 +0900

    tailwindcss 적용

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,74 +1,79 @@
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import "./navbar.css"
+import classNames from "classnames";
+import { Route } from "react-router-dom";
 
-function NavBar() {
-  const [click, setClick] = useState(false);
+export default function Navbar() {
+  const [menuToggle, setMenuToggle] = useState(false);
 
-  const handleClick = () => setClick(!click);
   return (
-    <>
-      <nav className="navbar">
-        <div className="nav-container">
-          <NavLink exact to="/" className="nav-logo">
-            Virtual Hertz
-            <i className="fas fa-code"></i>
-          </NavLink>
-
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/about"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/blog"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Blog
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/contact"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Contact Us
-              </NavLink>
-            </li>
-          </ul>
-          <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+    <nav className='bg-gray-100'>
+      <div className='mx-auto px-4'>
+        <div className='flex justify-between'>
+          {/* x는 수평방향 조절, y는 수직방향 조절 */}
+          <div className='flex space-x-4'>
+            <div>
+              
+                <a className='flex items-center py-5 px-2 text-gray-700 font-bold'>Home</a>
+            </div>
+            {/* sm크기 일경우엔 안보이고, md크기 부터는 display:flex 적용 */}
+            <div className="hidden md:flex items-center space-x-1">
+              
+                <a className='py-5 px-2 text-gray-700 font-bold hover:text-gray-900'>Contact Us</a>
+              
+                <a className='py-5 px-2 text-gray-700 font-bold hover:text-gray-900'>게시판</a>
+            </div>
+          </div>
+          <div className="hidden md:flex items-center space-x-1">
+            <div className='py-2 px-3 bg-yellow-400 hover:bg-yellow-300 text-yellow-900 hover:text-yellow-800 rounded transition duration-300'>
+            
+                <a>로그인</a>
+            </div>
+          </div>
+          <div className='md:hidden flex items-center'>
+            {!menuToggle ? (
+              <AiOutlineMenu onClick={() => setMenuToggle(!menuToggle)}
+                className="hover:cursor-pointer" />
+            ) : (<AiOutlineClose onClick={() => setMenuToggle(!menuToggle)}
+              className="hover:cursor-pointer" />)}
           </div>
         </div>
-      </nav>
-    </>
-  );
-}
+      </div>
 
-export default NavBar;
+      {/* mobile menu items */}
+      <div className={classNames("md:hidden", { hidden: !menuToggle })}>
+        
+          <a className='block py-2 px-4 text-sm hover:bg-gray-200'>Contact Us</a>
+        
+          <a className='block py-2 px-4 text-sm hover:bg-gray-200'>게시판</a>
+        
+          <a className='block py-2 px-4 text-sm bg-yellow-400 hover:bg-yellow-300'>로그인</a>
+
+      </div>
+    </nav>
+  )
+    // <div className={styles.navbarWrap}>
+    //   <ul className={styles.navbarContainer}>
+    //     <li className={styles.title}>
+    //       <Route href="/">
+    //         <a>Home</a>
+    //       </Route>
+    //     </li>
+    //     <li className={styles.title}>
+    //       <Route href="/contact">
+    //         <a>Contact Us</a>
+    //       </Route>
+    //     </li>
+    //     <li className={styles.title}>
+    //       <Route href="/boards">
+    //         <a>게시판</a>
+    //       </Route>
+    //     </li>
+    //     <li className={styles.title}>
+    //       <Route href="/login">
+    //         <a>로그인</a>
+    //       </Route>
+    //     </li>
+    //   </ul>
+    // </div>
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Switch>
-        {/* <Route exact path="/" component={Demo} /> */}
-        <Route path="/" component={App} />
-        {/* <Route path="/contain" component={PageContain} /> */}
-      </Switch>
-    </div>
-  </BrowserRouter>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
