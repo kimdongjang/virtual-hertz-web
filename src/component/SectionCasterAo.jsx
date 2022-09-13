1af6d3686a439b047db5e994fb49bc62a1d41682
JeonWoonKi <naru_oo@naver.com>
2022-09-13 23:52:38 +0900

    caster card 작업중

--- src/component/SectionCasterAo.jsx
+++ src/component/SectionCasterAo.jsx
@@ -32,35 +32,36 @@ const EqueCasterIntroduceHighlight = tw.p`
 const CasterSectionInner = tw.div`
     flex flex-col justify-center 
 `
+const CasterImageWrapper = tw.div`
+    flex
+`
 
 const CasterImage = tw.img`
-    m-8
+    h-18 m-8
 `
 
 
 const SectionCasterAo = forwardRef((prpos, ref) => {
     return (
         <SectionWrapper ref={ref}>
-            <div className="caster-section__card">
-                <div className="caster-section__img">
-                    <CasterImage src="./images/caster/ao1.png" />
+            <CasterImageWrapper>
+                <CasterImage src="./images/caster/ao1.png" />
+            </CasterImageWrapper>
+            <CasterSectionInner>
+                <div className="caster-section__title">
+                    <div className="caster-section__title-deco" style={{ background: '#CCDFF4' }} />
+                    <h2 className="">AO</h2>
                 </div>
-                <CasterSectionInner>
-                    <div className="caster-section__title">
-                        <div className="caster-section__title-deco" style={{ background: '#CCDFF4' }} />
-                        <h2 className="">AO</h2>
-                    </div>
-                    <p className="">Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum
-                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.</p>
-                    <div>
-                        <ReactAudioPlayer
-                            src="./audio/ao.mp3" controls
-                        />
-                    </div>
-                </CasterSectionInner>
-            </div>
+                <p className="">Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum
+                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.</p>
+                <div>
+                    <ReactAudioPlayer
+                        src="./audio/ao.mp3" controls
+                    />
+                </div>
+            </CasterSectionInner>
         </SectionWrapper>
     )
 }
