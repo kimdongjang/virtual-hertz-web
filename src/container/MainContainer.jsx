948f23247118602ee5636957725941b52d9fb68a
JeonWoonKi <naru_oo@naver.com>
2022-09-20 20:15:49 +0900

    scroll update

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -43,6 +43,17 @@ export default function MainContainer() {
 
   let cur = 0;
 
+  var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+  function preventDefault(e) {
+    e.preventDefault();
+  }
+
+  function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  }
 
   useEffect(() => {
     const wheelHandler = (e) => {
@@ -116,10 +127,29 @@ export default function MainContainer() {
         // }
       }
     };
+
+    var supportsPassive = false;
+    try {
+      window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+      }));
+    } catch (e) { }
+
+    var wheelOpt = supportsPassive ? { passive: false } : false;
+
     const outerDivRefCurrent = outerDivRef.current;
     outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    // window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
     return () => {
       outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
+      window.removeEventListener('DOMMouseScroll', preventDefault, false);
+      // window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+      window.removeEventListener('touchmove', preventDefault, wheelOpt);
+      window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
     };
   }, []);
 
